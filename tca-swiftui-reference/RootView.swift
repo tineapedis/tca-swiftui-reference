b5b00b1dbdeb5d911abd6d6339f8ffb41dc94185
nmurata <nmurata73@gmail.com>
2021-09-05 13:32:11 +0900

    create row view

--- tca-swiftui-reference/RootView.swift
+++ tca-swiftui-reference/RootView.swift
@@ -7,26 +7,63 @@
 
 import SwiftUI
 
+protocol RowType {
+    var title: String { get }
+}
+
+enum TcaRowType: RowType, CaseIterable, Hashable {
+    case overview
+
+    var title: String {
+        switch self {
+        case .overview:
+            return "概要"
+        }
+    }
+}
+
+enum SwiftuiRowType: RowType, CaseIterable, Hashable {
+    case overview
+
+    var title: String {
+        switch self {
+        case .overview:
+            return "概要"
+        }
+    }
+}
+
 struct RootView: View {
+    let tcaRowTypeList = TcaRowType.allCases
+    let swiftuiRowTypeList = SwiftuiRowType.allCases
+
     var body: some View {
         NavigationView {
             List {
                 Section(header: Text("The Composable Architecture"), content: {
-                    HStack {
-                        Image(systemName: "eyes")
-                        Text("概要")
+                    ForEach(tcaRowTypeList, id: \.self) { rowType in
+                        Row(rowType: rowType)
                     }
                 })
                 Section(header: Text("SwiftUI"), content: {
-                    HStack {
-                        Image(systemName: "eyes")
-                        Text("概要")
+                    ForEach(swiftuiRowTypeList, id: \.self) { rowType in
+                        Row(rowType: rowType)
                     }
                 })
             }
             .navigationBarTitle("TCA&SwiftUI Reference App", displayMode: .inline)
         }
+    }
+}
 
+struct Row: View {
+    var rowType: RowType
+
+    var body: some View {
+        HStack {
+            Image(systemName: "eyes")
+            Text(rowType.title)
+        }
     }
 }
 
